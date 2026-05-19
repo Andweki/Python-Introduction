05c6745a00983af2bbee4728c76adb76983db060
Andweki <andweki@gmail.com>
2026-05-19 14:20:32 +0300

    Input and Operators

---
+++ Input.py
@@ -0,0 +1,6 @@
+#user input allows users to enter data into a program.
+name = input("enter your name: ")
+print(name)
+
+age = input("enter your age: ")
+print(age)
---
+++ Operators.py
@@ -0,0 +1,39 @@
+#Operators are symbols used to perform operations in python
+
+#Arithmetic operators are used to for calculations
+x = 10
+y = 5
+
+print(x + y) #addition
+print(x - y) #subtraction
+print(x * y) #multiplication
+print(x / y) #division
+print(x % y) #modulus
+print (10 %4) #modulus gives the remainder of a division
+print(x ** y) #exponentiation
+
+#comparison operators are used to compare values return true or false
+
+#Greater than
+print (10>5) #greater than
+print(5>20) #less than
+
+#less than
+print(6<12) #less than
+print(50<10) #greater than
+
+#equal to
+print(5==5) #equal to
+print(3==7) #not equal to
+
+#not equal to
+print(10!=9) #not equal to
+print(7!=7) #equal to
+
+#greater than or equal to
+print(5>=4) #greater than or equal to
+print(9>=9) #greater than or equal to
+
+#less than or equal to
+print(6>=10) #less than or equal to
+print(30<=15) #greater than or equal to
