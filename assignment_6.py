99026da3ea37b8068bbbe36d7da3c3edfb6e4c1f
Andweki <andweki@gmail.com>
2026-06-12 09:35:49 +0300

    assigment 6

---
+++ assignment_6.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+marks= np.array([45, 67, 89, 32, 76, 50,])
+print(np.mean(marks))
+print(np.median(marks))
+print(np.std(marks))
+
+#Pie Chart
+subjects = ["Maths", "English", "Science", "Kiswahili", "ICT" ]
+students = [12, 8, 10, 6, 4]
+dist = [30, 20, 25, 15, 10]
+
+plt.pie(dist,labels=subjects,autopct="%1.1f%%")
+plt.title("Favorite subjects")
+plt.show()
+
+#Bar graph
+Days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+sales= [1200, 1500, 1800, 1400, 2000]
+
+plt.bar(Days, sales)
+plt.title("Weekly sales perfomance")
+plt.xlabel("Days")
+plt.ylabel("Sales")
+plt.show()
+
--- matplotlib_project.py
+++ matplotlib_project.py
@@ -26,7 +26,7 @@ plt.show()
 
 #scatter plot (study hours vs marks)
 study_hours=[1,2,3,4,5] #x-axis (independent variable)
-scores=[40,50,65,80,95] #
+scores=[40,50,65,80,95] #y-axis
 
 plt.scatter(study_hours, scores)
 plt.title("study hours vs marks")
