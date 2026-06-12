1a3269d04c2c0b9804d2bdc7c13b985db1983051
Andweki <andweki@gmail.com>
2026-06-12 09:37:21 +0300

    matplotlib

--- matplotlib_project.py
+++ matplotlib_project.py
@@ -26,7 +26,7 @@ plt.show()
 
 #scatter plot (study hours vs marks)
 study_hours=[1,2,3,4,5] #x-axis (independent variable)
-scores=[40,50,65,80,95] #y-axis
+scores=[40,50,65,80,95] #y-axis (independent variable)
 
 plt.scatter(study_hours, scores)
 plt.title("study hours vs marks")
